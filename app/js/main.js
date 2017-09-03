2594aaa603667ed280c314dd6b24a9a36537018b
Denis Yaroshenko <denyaroshenko@gmail.com>
2017-09-03 16:01:11 +0500

    Работа над адаптивом

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,33 @@
 // Дропдаун
-// $(document).ready(function(){
+$(document).ready(function(){
+		var window_width = $(document).width();
+		$(window).resize(function(){
+			window_width = $(document).width();
+		});
 
-//   $('.main-nav__item').hover(function () {
-//     clearTimeout($.data(this,'timer'));
-//     $('ul',this).stop(true,true).show();
-//   }, function () {
-//     $.data(this,'timer', setTimeout($.proxy(function() {
-//       $('ul',this).stop(true,true).hide();
-//     }, this), 10));
-//   });
-// });
+		if (window_width <=768) {
+				$('.main-nav__item').click(function () {
+					clearTimeout($.data(this,'timer'));
+					$('ul',this).stop(true,true).slideToggle(300);
+				}, function () {
+					$.data(this,'timer', setTimeout($.proxy(function() {
+						$('ul',this).stop(true,true).slideToggle(300);
+					}, this), 10));
+				});
+			}
+			else{
+				$('.main-nav__item').hover(function () {
+					clearTimeout($.data(this,'timer'));
+					$('ul',this).stop(true,true).slideToggle(300);
+				}, function () {
+					$.data(this,'timer', setTimeout($.proxy(function() {
+						$('ul',this).stop(true,true).slideToggle(300);
+					}, this), 10));
+				});
+			}
+  
+  $('.main-nav__mobile').click(function(){
+  	$('.main-nav__list').toggle('slow');
+  	// console.log('Открыть меню');
+  })
+});
